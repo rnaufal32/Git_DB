dd59caf876ab0ffbef585b68a6a3e8c68a5c280f
ZANR <62210020+Z4nR@users.noreply.github.com>
2020-11-28 01:19:02 +0700

    gituserDB

--- app/src/main/java/com/zulham/githubusersearch/Database/helper/MappingHelper.kt
+++ app/src/main/java/com/zulham/githubusersearch/Database/helper/MappingHelper.kt
@@ -1,4 +1,23 @@
 package com.zulham.githubusersearch.Database.helper
 
+import android.database.Cursor
+import com.zulham.githubusersearch.Database.db.DatabaseContract
+import com.zulham.githubusersearch.Database.entity.FavUser
+
 object MappingHelper {
+
+    fun mapCursorToArrayList(favsCursor: Cursor?): ArrayList<FavUser> {
+        val favsList = ArrayList<FavUser>()
+        favsCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.FavColumns._ID))
+                val imgUser = getString(getColumnIndexOrThrow(DatabaseContract.FavColumns.IMG_USER))
+                val userID = getInt(getColumnIndexOrThrow(DatabaseContract.FavColumns.USER_ID))
+                val UserName = getString(getColumnIndexOrThrow(DatabaseContract.FavColumns.USER_NAME))
+                favsList.add(FavUser(id, imgUser, userID, UserName))
+            }
+        }
+        return favsList
+    }
+
 }
